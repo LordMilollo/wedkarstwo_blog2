d868afa96522ef8534ea22c2c19189f22401fee0
LordMilollo <115570367+LordMilollo@users.noreply.github.com>
2024-06-12 13:49:33 +0200

    Update settings.py
    Dodanie opcji static

--- wedkarstwo_blog/settings.py
+++ wedkarstwo_blog/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'wedkarstwo_blog.urls'
@@ -125,6 +126,7 @@ USE_TZ = True'''
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
